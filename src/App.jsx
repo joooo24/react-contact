e2e3290d0a24f7f05f513189ef01752811874c9e
claire <61533589+joooo24@users.noreply.github.com>
2025-11-02 23:18:14 +0900

    add: 컨택트폼 인풋 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
+import ConctactList from './components/ConctactList'
+import ContactForm from './components/ContactForm'
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <h2>연락처 앱</h2>
+      <ContactForm></ContactForm>
+      <ConctactList></ConctactList>
+    </div>
   )
 }
 
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+// MUI의 Box, Button, TextField 사용
+import { Box, Button, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 const ContactForm = () => {
+  // useState로 이름/전화번호 상태 관리
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
   return (
-    <div>ContactForm</div>
-  )
-}
+    // Box를 flex 컨테이너로 세로 배치, 중앙 정렬
+    <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
+      {/* 두 개의 TextField(이름, 전화번호)와 "추가" 버튼 */}
+      {/* 각 입력 필드에 onChange 핸들러로 상태 업데이트 */}
+      <TextField
+        id="name"
+        label="이름"
+        variant="outlined"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <TextField
+        id="phone-number"
+        label="전화번호"
+        variant="outlined"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
+      <Button variant="contained" size="large">
+        추가
+      </Button>
+    </Box>
+  );
+};
 
-export default ContactForm
+export default ContactForm;

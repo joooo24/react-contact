abe74853847a4c6f4c8d0572133a2113f11c2d94
claire <61533589+joooo24@users.noreply.github.com>
2025-11-03 01:06:48 +0900

    create: 리스트 생성

--- src/components/ConctactList.jsx
+++ src/components/ConctactList.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import usePhoneBookStore from '../stores/usePhoneBookStore'
+import './ConctactList.css'
 
 const ConctactList = () => {
+    const {phoneBook, deleteContact} = usePhoneBookStore()
+
   return (
-    <div>ConctactList</div>
+    <div className="contact-list">
+      <ul className="contact-list__list">
+        {phoneBook.map((contact) => (
+            <li key={contact.id} className="contact-list__item">
+              <div className="contact-list__item-content">
+                <div className="contact-list__item-info">
+                  <span className="contact-list__item-id">
+                    ID: {contact.id}
+                  </span>
+                  <span className="contact-list__item-name">
+                    {contact.name}
+                  </span>
+                  <span className="contact-list__item-phone">
+                    {contact.phoneNumber}
+                  </span>
+                </div>
+                <button 
+                  className="contact-list__delete-button"
+                  onClick={() => deleteContact(contact.id)}
+                >
+                  삭제
+                </button>
+              </div>
+            </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
